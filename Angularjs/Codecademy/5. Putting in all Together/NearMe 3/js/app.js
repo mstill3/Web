9c9ab189593d38ff767b0fa564163b588566921e
Matt Stillwell <18670089+mstill3@users.noreply.github.com>
2018-07-20 11:59:40 -0400

    Finished codecademy

---
+++ Angularjs/Codecademy/5. Putting in all Together/NearMe 3/js/app.js
@@ -0,0 +1,19 @@
+/*global angular*/
+/*eslint-disable no-unused-vars*/
+
+var app = angular.module('NearMeApp', ['leaflet-directive', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'MainController',
+            templateUrl: 'views/main.html'
+        })
+        .when('/about', {
+            controller: 'AboutController',
+            templateUrl: 'views/about.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
---
+++ Angularjs/Codecademy/5. Putting in all Together/NearMe 3/js/controllers/AboutController.js
@@ -0,0 +1,5 @@
+/*global app*/
+
+app.controller('AboutController', ['$scope', function ($scope) {
+
+}]);
