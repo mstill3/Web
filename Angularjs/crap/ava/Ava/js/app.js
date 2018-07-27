dc49db1d78233bca0588b9832c1f045ecee5aa1a
Matt Stillwell <stillwell006@gmail.com>
2018-07-27 17:10:46 -0400

    Added Witch-Hunt

---
+++ Angularjs/crap/ava/Ava/js/app.js
@@ -0,0 +1,19 @@
+/*global angular*/
+/*eslint-disable no-unused-vars*/
+
+var app = angular.module('GalleryApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'views/home.html'
+        })
+        .when('/photos/:id', {
+            controller: 'PhotoController',
+            templateUrl: 'views/photo.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
---
+++ Witch-Hunt/src/main/java/salem/salemApplication.java
@@ -0,0 +1,12 @@
+package salem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class salemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(salemApplication.class, args);
+	}
+}
