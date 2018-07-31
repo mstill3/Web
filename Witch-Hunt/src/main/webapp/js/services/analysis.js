c2ef8dd7e1af35130f54b699f8550cefec3775f7
Michael Odio <michaelfodio@gmail.com>
2018-07-31 13:22:50 -0400

    Added accurate video analysis

--- Witch-Hunt/src/main/webapp/js/services/analysis.js
+++ Witch-Hunt/src/main/webapp/js/services/analysis.js
@@ -1,7 +1,7 @@
 mainApp.factory('analysis', ['$http', function ($http) {
 
 //    return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json').then(
-    return $http.get('json/gold.json').then(
+    return $http.get('json/meta.json').then(
 	    //success
         function (response) {
             return response;
